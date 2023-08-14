63a947ad485fe90f7785ac302cc688c251360cc9
MichaelKarina <mikekarina68@gmail.com>
2023-08-14 08:46:08 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,30 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
- * struct dog - Define a new type struct dog
- * @name: names of dog
- * @age: AGE of dog
- * @owner: owner of dog
- * 
- * Description: Data structure for holbertn and Django
+ * Dog_ti -typedef for struct dog
  */
-typedef struct dog
+typedef struct dog dog_t;
+
+/**
+ * struct dogs- struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner:owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
-
-typedef dog dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
