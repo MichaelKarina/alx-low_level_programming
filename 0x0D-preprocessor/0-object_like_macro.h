ece7b0a4719e78dd6c00bba50f914dcd3a06b042
MichaelKarina <mikekarina68@gmail.com>
2023-08-14 08:14:10 -0700

    preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * File:0-Object_like_macro.h
+ * Auth: Michael Karina 
+ * Desc: Header file that defines a macro named
+ *    size as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
